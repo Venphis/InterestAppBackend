b805a51be1962a68268cef2d62aa23b35f5cddbd
bart-soj <bartoszsojka2@gmail.com>
2025-11-26 22:28:58 +0100

    mend

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -59,6 +59,12 @@ const fetchChats = async (req, res, next) => {
             .lean(); 
 
         const validChats = chats.filter(chat => chat.participants && chat.participants.length > 1);
+        const validChats = validChats.map { chat =>
+            ..chat,
+            participants: chat.participants.map { participant =>
+                participant._id
+            }
+        }
         res.status(200).json(validChats);
     } catch (error) {
         console.error('[chatCtrl] Fetch Chats Error:', error);
