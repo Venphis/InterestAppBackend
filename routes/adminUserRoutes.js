f745155d971f0eb2c6717ab43416c21d0ca1a81b
Venphis <165069478+Venphis@users.noreply.github.com>
2026-01-28 14:05:22 +0100

    małe zmiany

--- routes/adminUserRoutes.js
+++ routes/adminUserRoutes.js
@@ -50,8 +50,8 @@ router.route('/:userId')
 
 // User Actions
 router.put('/:userId/ban', authorizeAdminRole(['admin', 'superadmin']), banValidation, banUser);
-router.put('/:userId/unban', authorizeAdminRole(['admin', 'superadmin']), userIdValidation, unbanUser);
-router.put('/:userId/restore', authorizeAdminRole(['superadmin']), userIdValidation, restoreUser);
+router.put('/:userId/unban', authorizeAdminRole(['admin', 'superadmin', 'moderator']), userIdValidation, unbanUser);
+router.put('/:userId/restore', authorizeAdminRole(['admin','superadmin']), userIdValidation, restoreUser);
 router.put('/:userId/verify-email', authorizeAdminRole(['admin', 'superadmin']), userIdValidation, manuallyVerifyEmail);
 router.put('/:userId/role', authorizeAdminRole(['superadmin']), roleChangeValidation, changeUserRole);
 
